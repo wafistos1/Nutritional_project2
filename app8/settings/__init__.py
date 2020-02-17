e16936b7f877ccd69af15874bbd49072076f9f49
wafistos1 <mameri.wafi@gmail.com>
2020-02-17 18:41:15 +0100

    git delete config of sentry in settings

--- app8/settings/__init__.py
+++ app8/settings/__init__.py
@@ -32,15 +32,7 @@ SECRET_KEY = 'ol&r6n+p7x2veeouz12e8+9v+muuv#a$(31t_0%c#6#f5zdwz)+=' # developmen
 >>> "".join([random.choice(string.printable) for _ in range(24)])
 
 """
-# Sentry configure the SDK
-# sentry_sdk.init(
-#     dsn="https://83d478e741f4470888b293a7cb4277a2@sentry.io/2544798",
-#     integrations=[DjangoIntegration()],
 
-#     # If you wish to associate users to errors (assuming you are using
-#     # django.contrib.auth) you may enable sending PII data.
-#     send_default_pii=True
-# )
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
